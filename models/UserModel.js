b661bcc6e443ea3a363346221855baefc96efdd6
NoobClearZ <yudhapraseya1754@gmail.com>
2023-02-07 15:05:53 +0700

    ibox

---
+++ models/UserModel.js
@@ -0,0 +1,26 @@
+import { Sequelize } from 'sequelize'
+
+import db from '../config/Database.js'
+
+const Users = db.define('users',{
+    name:{
+        type: Sequelize.STRING
+    },
+    email:{ 
+        type: Sequelize.STRING
+    },
+    password:{
+        type: Sequelize.STRING
+    },
+    refresh_token:{
+        type: Sequelize.TEXT
+    }    
+},{
+    freezeTableName:true
+});
+
+(async () => {
+    await db.sync();
+})()
+
+export default Users;
